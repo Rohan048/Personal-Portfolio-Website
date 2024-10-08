d60057d561ea0dc8b489a94620445055bdb3ddec
rohan048 <rohanrai042003@gmail.com>
2024-10-08 17:40:35 +0530

    Portfolio

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission for demo purposes
+
+    // Simple alert on form submission
+    alert('Message sent successfully!');
+});
